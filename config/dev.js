7edc2b450957b80f8172b799ad9349158d7bc425
DimRev <dimarev444@gmail.com>
2024-01-11 08:55:11 +0200

    trying to deploy again

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@
 export default {
     // dbURL: `mongodb+srv://Admin:${ADMIN_PASSWORD}@mistertoy.ku3ejxj.mongodb.net/`,
     // dbURL: 'mongodb://localhost:27017',
-    dbURL: `mongodb+srv://admin:${process.env.ADMIN_PASSWORD||ADMIN_PASSWORD}@cluster0.1prbfc9.mongodb.net//`,
+    dbURL: `mongodb+srv://admin:${process.env.ADMIN_PASSWORD}@cluster0.1prbfc9.mongodb.net//`,
     dbName: 'monday_db',
 }
