4567f51aa60352925ebdc1564b4a66e0e21f8b18
DimRev <dimarev444@gmail.com>
2024-01-11 08:46:24 +0200

    Hide local import

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
-import { ADMIN_PASSWORD } from "../globalVars.js";
+// import { ADMIN_PASSWORD } from "../globalVars.js";
 
 export default {
-    // dbURL: `mongodb+srv://Admin:${ADMIN_PASSWORD}@mistertoy.ku3ejxj.mongodb.net/`,
+    dbURL: `mongodb+srv://Admin:${ADMIN_PASSWORD}@mistertoy.ku3ejxj.mongodb.net/`,
     // dbURL: 'mongodb://localhost:27017',
     dbURL: `mongodb+srv://admin:${ADMIN_PASSWORD}@cluster0.1prbfc9.mongodb.net//`,
     dbName: 'monday_db',
