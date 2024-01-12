380b734cbdfd1d43175655bb8695858cb1e91908
DimRev <dimarev444@gmail.com>
2024-01-12 12:56:49 +0200

    group socket implemented

--- api/group/group.controller.js
+++ api/group/group.controller.js
@@ -1,4 +1,5 @@
 import { loggerService } from '../../services/logger.service.js'
+import { socketService } from '../../services/socket.service.js'
 import { utilService } from '../../services/utils.service.js'
 import { groupService } from './group.service.js'
 
@@ -15,10 +16,17 @@ import { groupService } from './group.service.js'
 
 export async function addGroup(req, res) {
   const { boardId, group } = req.body
-
   const groupToAdd = { ...group }
+  const userId = req.loggedinUser
+
   try {
     const addedGroup = await groupService.add(boardId, groupToAdd)
+    socketService.broadcast({
+      type: 'add-group',
+      data: { boardId, group: addedGroup },
+      room: boardId,
+      userId,
+    })
     res.send(addedGroup)
   } catch (err) {
     loggerService.error('B.C | Error adding group ', err)
@@ -28,13 +36,21 @@ export async function addGroup(req, res) {
 
 export async function updateGroup(req, res) {
   const { boardId, groupId, group } = req.body
-  const groupToUpdate = group
+  const groupToUpdate = { ...group }
+  const userId = req.loggedinUser
+
   try {
     const updatedGroup = await groupService.update(
       boardId,
       groupId,
       groupToUpdate
     )
+    socketService.broadcast({
+      type: 'update-group',
+      data: { boardId, groupId, group: updatedGroup },
+      room: boardId,
+      userId,
+    })
     res.send(updatedGroup)
   } catch (err) {
     loggerService.error('B.C | Error adding group ', err)
@@ -44,8 +60,15 @@ export async function updateGroup(req, res) {
 
 export async function removeGroup(req, res) {
   const { boardId, groupId } = req.params
+  const userId = req.loggedinUser
   try {
     await groupService.remove(boardId, groupId)
+    socketService.broadcast({
+      type: 'remove-group',
+      data: { boardId, groupId },
+      room: boardId,
+      userId,
+    })
     res.send(groupId)
   } catch (err) {
     loggerService.error('B.C | Error removing group ', err)
